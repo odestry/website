36d5a66699cdff9a9a18fc491dc94ab988e8aeeb
Youness Idbakkasse <itsblanklob@gmail.com>
2023-05-06 03:18:58 +0200

    Fixed scene

--- app/components/scene.tsx
+++ app/components/scene.tsx
@@ -4,17 +4,14 @@ import {Sky, Environment, Cloud} from '@react-three/drei';
 
 export default function Scene() {
   return (
-    <div className="absolute inset-0 w-full h-screen -z-10">
+    <div className="absolute inset-0 w-full h-screen -z-10 bg-blue-100">
       <Canvas shadows camera={{position: [-50, -25, 120], fov: 75}}>
         <Suspense fallback={null}>
           <ambientLight intensity={0.6} />
-          <directionalLight
-            color="white"
-            position={[-10, -10, 0]}
-            intensity={2}
-          />
+          <directionalLight color="white" position={[10, 0, 0]} intensity={2} />
           <Cloud scale={8} position={[20, 0, 0]} />
           <Cloud scale={6} position={[-20, 10, 0]} />
+          <Cloud scale={4} position={[0, -10, 0]} />
           <Environment preset="city" />
           <Sky
             azimuth={0.1}
